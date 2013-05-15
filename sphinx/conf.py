16fc26df2162ac513e86e5265d6780c6e0759943
Reid Priedhorsky <reidpr@lanl.gov>
2013-05-14 18:09:54 -0600

    title docs with commit hash

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -103,7 +103,9 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "QUAC documentation"
+#
+# NOTE: This is set using -D in the Makefile to get the git commit hash.
+#html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
