53686b27dd63db589c4dd02762e58516e0d2e5fc
Reid Priedhorsky <reidpr@lanl.gov>
2014-03-04 14:06:01 -0700

    clean up & fix tests

--- misc/wp-preprocess.mk
+++ misc/wp-preprocess.mk
@@ -49,7 +49,7 @@ endef
 all: dircheck hashed metadata
 
 clean:
-	rm -Rf hashed hashed_small hashed_tiny metadata metadata.total.pkl.gz
+	rm -Rf hashed hashed_small hashed_tiny metadata metadata.total.pkl.gz missing.tsv
 
 dircheck:
 	test -d raw -a -d raw/2012 -a -d raw/2012/2012-10
